84f9a74a043858f1f496f67273938fde9f4e5545
Cl3arglass <haltaffer@gmail.com>
2022-03-14 10:11:42 -0700

    next link

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Link from 'next/link'
 
 export default function Page() {
   return (
     <main>
       <h1>Does it rewrite?</h1>
       <p>
-        Check out <a href="/api/event">/api/event</a>.
+        Check out 
+        <Link href="/api/event">
+          <a>/api/event</a>
+        </Link>
       </p>
     </main>
   )
