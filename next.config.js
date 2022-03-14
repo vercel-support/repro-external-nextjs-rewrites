814a56e7b385fca0ce001df33a30379e60ff05c7
Cl3arglass <haltaffer@gmail.com>
2022-03-14 09:39:48 -0700

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
+// module.exports = {
+//   i18n: {
+//     locales: ['en', 'de'],
+//     defaultLocale: 'en',
+//   },
+//   rewrites: [{
+//     source: '/api/event',
+//     destination: 'https://www.example.com/api/event',
+//   }],
+// };
 module.exports = {
   i18n: {
-    locales: ['en', 'de'],
-    defaultLocale: 'en',
+        locales: ['en', 'de'],
+        defaultLocale: 'en',
+      },
+      
+  async rewrites() {
+    return [
+      {
+        source: '/api/event',
+        destination: 'https://www.example.com/api/event',
+      },
+    ]
   },
-  rewrites: [{
-    source: '/api/event',
-    destination: 'https://www.example.com/api/event',
-  }],
-};
+}
